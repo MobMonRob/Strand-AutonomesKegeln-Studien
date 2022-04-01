41d9dae7dd8c3ed4b97ce6631a1e9c3c983f9f95
tisouz <tobiasstang21@gmail.com>
2022-04-01 08:13:59 -0700

    read sensordata and convert to pcl::PointCloud

--- ros/sphero_bolt_node/src/processing.cpp
+++ ros/sphero_bolt_node/src/processing.cpp
@@ -1,6 +1,10 @@
 #include <ros/ros.h>
 // PCL specific includes
+#include <sensor_msgs/PointCloud.h>
 #include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/point_cloud_conversion.h>
+
+        
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
@@ -10,12 +14,18 @@
 void 
 cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
 {
-  // Create a container for the data.
-  sensor_msgs::PointCloud2 output;
-  std::cout << "received something" << std::endl;
+  //convert input to pcl::PointCloud<pcl::PointXYZ>
+  pcl::PCLPointCloud2 pcl_pc2;
+  pcl_conversions::toPCL(*input,pcl_pc2);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr temp_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::fromPCLPointCloud2(pcl_pc2,*temp_cloud);
 
+  for (int i = 0; i < temp_cloud->points.size(); i++) {
 
+    std::cout << "x:\t" << temp_cloud->points[i].x << "\ty:\t" << temp_cloud->points[i].y << "\tz:\t" << temp_cloud->points[i].z << "\n";
+  }
 }
+
 
 int main (int argc, char** argv)
 {
@@ -23,9 +33,8 @@ int main (int argc, char** argv)
   ros::init (argc, argv, "sensor_processing");
   ros::NodeHandle nh;
 
-  std::cout << "wer das lies ist verwirrt!" << std::endl;
   // Create a ROS subscriber for the input point cloud
-  ros::Subscriber sub = nh.subscribe ("SENSORTOPICNAME", 1, cloud_cb);
+  ros::Subscriber sub = nh.subscribe ("cloud", 1, cloud_cb);
 
   // Spin
   ros::spin ();
